cb9692abea5e3995bcbc7868e744de36ef8d2afc
Santiago Rodríguez Romero <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-15 19:57:06 +0200

    Initial files

---
+++ test_V7.py
@@ -0,0 +1,16 @@
+import unittest
+import runFile
+
+
+class TestV7(unittest.TestCase):
+    def test_743(self):
+        tests = [
+            {"input": "1\n1\n0\n", "output": "0\n"},
+            {"input": "1\n2\n0\n", "output": "1\n"},
+            {"input": "3\n2 1 1\n0\n", "output": "1\n"},
+            {"input": "1\n1\n1\n2\n3\n2 1 1\n0", "output": "0\n1\n1\n"},
+        ]
+
+        for test in tests:
+            output, error = runFile.run_script("743.py", test["input"])
+            self.assertEqual(output, test["output"])
