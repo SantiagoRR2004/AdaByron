6ec9d8318e1c102859587e13ac6b1bed5a933917
Santiago Rodríguez Romero <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-16 18:15:13 +0200

    Simplified tests

--- test_V7.py
+++ test_V7.py
@@ -5,9 +5,9 @@ import runFile
 class TestV7(unittest.TestCase):
     def test_743(self):
         tests = [
-            {"input": "1\n1\n0\n", "output": "0\n"},
-            {"input": "1\n2\n0\n", "output": "1\n"},
-            {"input": "3\n2 1 1\n0\n", "output": "1\n"},
+            {"input": "1\n1\n0", "output": "0\n"},
+            {"input": "1\n2\n0", "output": "1\n"},
+            {"input": "3\n2 1 1\n0", "output": "1\n"},
             {"input": "1\n1\n1\n2\n3\n2 1 1\n0", "output": "0\n1\n1\n"},
         ]
 
