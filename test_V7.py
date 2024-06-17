d3d08a54c6647b0d7d70f6c584220e0e05c7c8b1
Santiago Rodríguez Romero <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-17 13:27:35 +0200

    Initial files

--- test_V7.py
+++ test_V7.py
@@ -14,3 +14,26 @@ class TestV7(unittest.TestCase):
         for test in tests:
             output, error = runFile.run_script("743.py", test["input"])
             self.assertEqual(output, test["output"])
+
+    def test_745_1(self):
+        tests = [
+            {
+                "input": "1 27 51 703 1000 0\n19397 940476005 0\n0",
+                "output": "A AA AY AAA ALL\nABRA CADABRA\n",
+            },
+            {"input": "1 27 51 703 1000 0\n0", "output": "A AA AY AAA ALL\n"},
+            {"input": "19397 940476005 0\n0", "output": "ABRA CADABRA\n"},
+        ]
+
+        for test in tests:
+            output, error = runFile.run_script("745.py", test["input"])
+            self.assertEqual(output, test["output"])
+
+    def test_745_2(self):
+
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for letter in alphabet:
+            input = f"{alphabet.index(letter)+1} 0\n0"
+            output = f"{letter}\n"
+            output, error = runFile.run_script("745.py", input)
+            self.assertEqual(output, output)
