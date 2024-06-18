9b6ebba7867a9be9519e9078062aa6d24176496c
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-18 12:13:06 +0200

    Working code

--- test_V7.py
+++ test_V7.py
@@ -50,3 +50,18 @@ class TestV7(unittest.TestCase):
             output = f"{letter}\n"
             output, error = runFile.run_script("745.py", input)
             self.assertEqual(output, output)
+
+    def test_746(self):
+        tests = [
+            {
+                "input": "5 10\n1 9 2 8 5\n5 10\n5 8 2 9 1\n5 10\n1 2 3 4 5\n0 0",
+                "output": "2\n2\n0\n",
+            },
+            {"input": "5 10\n1 9 2 8 5\n0 0", "output": "2\n"},
+            {"input": "5 10\n5 8 2 9 1\n0 0", "output": "2\n"},
+            {"input": "5 10\n1 2 3 4 5\n0 0", "output": "0\n"},
+        ]
+
+        for test in tests:
+            output, error = runFile.run_script("746.py", test["input"])
+            self.assertEqual(output, test["output"])
