d66e73c458f17982a003b7c17873352bb3d2667c
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-25 16:17:35 +0200

    Process inputs

---
+++ RegGalicia2024D.py
@@ -0,0 +1,6 @@
+numCromos, numSobres, precioSobre = [int(x) for x in input().split()]
+
+preciosCromo = [int(x) for x in input().split()]
+
+for i in range(numSobres):
+    cromos = [int(x) for x in input().split()]
