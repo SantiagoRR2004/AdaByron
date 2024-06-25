89c75e77c14319790a48275850d7d5ad172e0769
Santiago Rodríguez Romero <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-25 16:26:50 +0200

    Process input

--- RegGalicia2024G.py
+++ RegGalicia2024G.py
@@ -1,1 +1,10 @@
 # https://en.wikipedia.org/wiki/Bipartite_graph#Testing_bipartiteness
+
+nCasos = int(input())
+
+for _ in range(nCasos):
+
+    nAmigos, nRivalidades = [int(x) for x in input().split()]
+
+    for _ in range(nRivalidades):
+        per1, per2 = [int(x) for x in input().split()]
