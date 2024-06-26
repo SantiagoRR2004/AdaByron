ec32ec139dd71c4dfa2b2b09812ec3a7558b5fb2
usbt0p <lucascdeaa@gmail.com>
2024-06-26 20:13:22 +0200

    No enviado

--- RegGalicia2024D.py
+++ RegGalicia2024D.py
@@ -2,5 +2,23 @@ numCromos, numSobres, precioSobre = [int(x) for x in input().split()]
 
 preciosCromo = [int(x) for x in input().split()]
 
+sobres = [
+    [int(x) for x in input().split()] 
+    for _ in range(numSobres)
+    ]
+
+faltantes = [True] * numCromos
+valSobres = 0
+resultados = []
+
 for i in range(numSobres):
-    cromos = [int(x) for x in input().split()]
+    valSobres += precioSobre
+
+    for cromo in sobres[i]:
+        faltantes[cromo-1] = False
+
+    valFaltantes = sum([j for idx, j in enumerate(preciosCromo) if faltantes[idx]])
+
+    resultados.append(valSobres + valFaltantes)
+
+print(min(resultados))
