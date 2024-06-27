c6366da7f8757ec0ab6cf2c15eaa2527758c76a6
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-27 13:51:01 +0200

    Tiny improvement and new test

--- RegGalicia2024D.py
+++ RegGalicia2024D.py
@@ -2,23 +2,38 @@ numCromos, numSobres, precioSobre = [int(x) for x in input().split()]
 
 preciosCromo = [int(x) for x in input().split()]
 
-sobres = [
-    [int(x) for x in input().split()] 
-    for _ in range(numSobres)
-    ]
 
-faltantes = [True] * numCromos
-valSobres = 0
-resultados = []
+precioMin = min(precioSobre * numSobres, sum(preciosCromo))
+cromosObtenidos = set()
+
 
 for i in range(numSobres):
-    valSobres += precioSobre
+    cromos = [int(x) for x in input().split()]
+    cromosObtenidos.update(cromos)
+    costoSobres = (i + 1) * precioSobre
+    costoCromos = sum(
+        preciosCromo[cromo - 1]
+        for cromo in (set(range(1, numCromos + 1)) - cromosObtenidos)
+    )
+    precioMin = min(precioMin, costoSobres + costoCromos)
 
-    for cromo in sobres[i]:
-        faltantes[cromo-1] = False
+print(precioMin)
 
-    valFaltantes = sum([j for idx, j in enumerate(preciosCromo) if faltantes[idx]])
 
-    resultados.append(valSobres + valFaltantes)
+# sobres = [[int(x) for x in input().split()] for _ in range(numSobres)]
 
-print(min(resultados))
+# faltantes = [True] * numCromos
+# valSobres = 0
+# resultados = []
+
+# for i in range(numSobres):
+#     valSobres += precioSobre
+
+#     for cromo in sobres[i]:
+#         faltantes[cromo - 1] = False
+
+#     valFaltantes = sum([j for idx, j in enumerate(preciosCromo) if faltantes[idx]])
+
+#     resultados.append(valSobres + valFaltantes)
+
+# print(min(resultados))
