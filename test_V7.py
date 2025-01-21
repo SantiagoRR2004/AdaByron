b837ba1525b37188a245b1002501c2d29344b017
Santiago Rodríguez Romero <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-21 18:28:44 +0100

    Changed test format

--- test_V7.py
+++ test_V7.py
@@ -301,23 +301,3 @@ TTTTTT""",
         for test in tests:
             output, error = runFile.run_script("758.py", test["input"])
             self.assertEqual(output, test["output"])
-
-    def test_762(self):
-        tests = [
-            {
-                "input": """500 200 250 400
-500 200 300 400
-200 500 400 300""",
-                "output": """NO
-SI
-SI
-""",
-            },
-            {"input": "500 200 250 400", "output": "NO\n"},
-            {"input": "500 200 300 400", "output": "SI\n"},
-            {"input": "200 500 400 300", "output": "SI\n"},
-        ]
-
-        for test in tests:
-            output, error = runFile.run_script("762.py", test["input"])
-            self.assertEqual(output, test["output"])
