47a33826a65624f88e6a061a53d0be303d6c6410
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-02-09 16:50:41 +0100

    Changed 742 format

--- test_V7.py
+++ test_V7.py
@@ -3,22 +3,6 @@ import runFile
 
 
 class TestV7(unittest.TestCase):
-    def test_742(self):
-        tests = [
-            {"input": "1\n010101", "output": "Girar\n"},
-            {"input": "1\n000111", "output": "No girar\n"},
-            {"input": "1\n0011", "output": "Da igual\n"},
-            {"input": "2\n010101\n000111", "output": "Girar\nNo girar\n"},
-            {
-                "input": "3\n010101\n000111\n0011",
-                "output": "Girar\nNo girar\nDa igual\n",
-            },
-        ]
-
-        for test in tests:
-            output, error = runFile.run_script("742.py", test["input"])
-            self.assertEqual(output, test["output"])
-
     def test_743(self):
         tests = [
             {"input": "1\n1\n0", "output": "0\n"},
