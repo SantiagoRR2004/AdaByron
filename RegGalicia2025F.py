8eecabeb1238479fc4eefffaced14a00b408999a
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-06-01 12:41:28 +0200

    Solve problem

--- RegGalicia2025F.py
+++ RegGalicia2025F.py
@@ -1,3 +1,5 @@
+# https://en.wikipedia.org/wiki/Continuous_knapsack_problem
+
 nCases = int(input())
 
 for _ in range(nCases):
@@ -6,42 +8,19 @@ for _ in range(nCases):
     times = [int(x) for x in input().split()]
     valoresAbs = [int(x) for x in input().split()]
 
+    activities = sorted(  # value by time, time and popularity
+        [(valoresAbs[i] / times[i], times[i], valoresAbs[i]) for i in range(nStreams)],
+        reverse=True,
+    )
 
-    vs = [v/t for v,t in zip(valoresAbs,times)]
-    ind = {}
-    for i in range(nStreams):
-        ind[times[i]] = i
+    totalValue = 0
 
-    ind2 = {}
-    for i in range(nStreams):
-        ind2[valoresAbs[i]] = i
+    for ratio, duration, activityValue in activities:
+        if nTime >= duration:
+            totalValue += activityValue
+            nTime -= duration
+        else:
+            totalValue += ratio * nTime
+            break
 
-    times.sort(key=lambda k:vs[ind[k]],reverse=True)
-    valoresAbs.sort(key=lambda k:vs[ind2[k]],reverse=True)
-    vs.sort(reverse=True)
-
-    value = 0
-
-    counter = 0
-    while nTime != 0 and counter < nStreams:
-        maxTime = min(nTime, times[counter])
-
-        value += maxTime*valoresAbs[counter]/times[counter]
-        nTime -= maxTime
-
-        counter += 1
-
-    print(f"{round(value,4):.4f}")
-
-
-"""3
-4 125
-16 27 38 41
-30 75 105 180
-5 67
-11 22 31 41 54
-25 60 90 120 200
-3 5
-12 24 36
-47 97 140
-"""
+    print(f"{totalValue:.4f}")
