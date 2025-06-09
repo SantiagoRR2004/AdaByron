e82bb49fa0612a52db5ec937ff25870f3e1bdc47
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-06-09 19:01:10 +0200

    Correct solution

--- RegGalicia2025D.py
+++ RegGalicia2025D.py
@@ -1,33 +1,34 @@
-n_asc, n_pisos =  [int(x) for x in input().split()]
-g = {i: {i+1 :1} for i in range(1, n_pisos +1)}
+# https://cp-algorithms.com/graph/01_bfs.html
+
+
+n_asc, n_pisos = [int(x) for x in input().split()]
+g = {i: {i + 1: 1} for i in range(1, n_pisos + 1)}
 g[n_pisos] = {}
 
 for _ in range(n_asc):
-    p1, p2 = [x for x in input().split()]
+    p1, p2 = [int(x) for x in input().split()]
     g[p1][p2] = 0
 
 
-def dijstra(g, s):
-    d, prev = {}, {}
-    res = []
+def BFS01(g):
+    from collections import deque
 
-    for v in g:
-        d[v] = float("inf")
-        prev[v] = None
-    d[s] = 0
+    goal = len(g)
+    queue = deque([1])
+    dist = {node: float("inf") for node in g}
+    dist[1] = 0
 
-    q = [v for v in g]
+    while queue:
+        node = queue.popleft()
+        if node == goal:
+            return dist[goal]
+        for neighbor, weight in g[node].items():
+            if dist[node] + weight < dist[neighbor]:
+                dist[neighbor] = dist[node] + weight
+                if weight == 1:
+                    queue.append(neighbor)
+                else:
+                    queue.appendleft(neighbor)
 
-    while q:
-        u = min(q, key=d.get())
-        q.remove(u)
-        res.append(u)
 
-        for vecino in g[u]:
-            if vecino in q and d[vecino] > d[u] + g[u][vecino]:
-                d[vecino] = d[u]+ g[u][vecino]
-                prev[vecino] = u
-
-    return d
-
-print(dijstra(g, n_pisos))
+print(BFS01(g))
